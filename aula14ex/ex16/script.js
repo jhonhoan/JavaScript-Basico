8123728c3b1089dddc81a7ed4efbfb65ed649e0b
jhonatan <132399356+jhonhoan@users.noreply.github.com>
2024-07-10 16:28:41 -0300

    Refazendo o exercicio

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -5,36 +5,35 @@ function clicar()
     var passos = document.getElementById('passos')
     var res = document.getElementById('res')
 
-    if(inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0)
+    if(inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0 )
     {
-        res.innerHTML = 'Impossivel contar'
+        res.innerHTML = ' [ERRO] Impossivel contar!'
     }else{
-        res.innerHTML = 'Contando'
+        res.innerHTML = 'Contando: <br>'
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passos.value)
+
         if(p <= 0)
         {
-            alert('[ERRO] Considerando passos 1')
+            alert('Passo Invalido! Passo recebe "1"')
             p = 1
         }
 
-        if (i < f)
-        {
-            //contagem crescente
-            for(var c = i; c <= f; c += p)
-            {
-                res.innerHTML += `${c} `
-            }
-         }else
-         {
-            //contagem regressiva
+        if(i < f)
+        {   //contagem crescente
+            for(var c = i; c <= f; c +=p)
+                {
+                    res.innerHTML += `${c} `
+                    
+                }
+        }else
+        { //contagem regressiva
             for(var c = i; c >= f; c -= p)
             {
                 res.innerHTML += `${c} `
             }
-         }
-
+        }
+        
     }
-
 }
