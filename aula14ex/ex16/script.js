e940e6f0635e22859618c78edecfc22beae06b8b
jhonatan <132399356+jhonhoan@users.noreply.github.com>
2024-07-15 16:27:37 -0300

    .

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,39 +1,33 @@
-function clicar()
-{
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passos = document.getElementById('passos')
-    var res = document.getElementById('res')
+function clicar() {
+    var inicio = document.getElementById('inicio');
+    var fim = document.getElementById('fim');
+    var passos = document.getElementById('passos');
+    var res = document.getElementById('res');
 
-    if(inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0 )
-    {
-        res.innerHTML = ' [ERRO] Impossivel contar!'
-    }else{
-        res.innerHTML = 'Contando: <br>'
-        var i = Number(inicio.value)
-        var f = Number(fim.value)
-        var p = Number(passos.value)
+    if (inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0) {
+        res.innerHTML = "Não é possível contar! Por favor, preencha todos os campos.";
+    } else {
+        res.innerHTML = "Contando: <br>";
 
-        if(p <= 0)
-        {
-            alert('Passo Invalido! Passo recebe "1"')
-            p = 1
+        var i = Number(inicio.value);
+        var f = Number(fim.value);
+        var p = Number(passos.value);
+
+        if (p <= 0) {
+            alert("Passo inválido! Considerando passo 1.");
+            p = 1;
         }
 
-        if(i < f)
-        {   //contagem crescente
-            for(var c = i; c <= f; c +=p)
-                {
-                    res.innerHTML += `${c} `
-                    
-                }
-        }else
-        { //contagem regressiva
-            for(var c = i; c >= f; c -= p)
-            {
-                res.innerHTML += `${c} `
+        if (i < f) {
+            // Contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} `;
+            }
+        } else {
+            // Contagem decrescente
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} `;
             }
         }
-        
     }
 }
